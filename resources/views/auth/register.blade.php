58dc1f94c5a92c0f76e290d1a80d2f0010fa0c23
tbunsin <apps.bunsin@gmail.com>
2025-05-05 23:25:43 +0700

    space

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,6 +6,52 @@
         <main>
             <div class="container-xl px-4">
                 <div class="row justify-content-center">
+                    <!-- Create Organization-->
+                    <div class="col-xl-5 col-lg-6 col-md-8 col-sm-11 mt-4">
+                        <div class="card text-center h-100">
+                            <div class="card-body px-5 pt-5 d-flex flex-column">
+                                <div>
+                                    <div class="h3 text-primary">Create</div>
+                                    <p class="text-muted mb-4">Create an organization and invite new members</p>
+                                </div>
+                                <div class="icons-org-create align-items-center mx-auto mt-auto">
+                                    <i class="icon-users" data-feather="users"></i>
+                                    <i class="icon-plus fas fa-plus"></i>
+                                </div>
+                            </div>
+                            <div class="card-footer bg-transparent px-5 py-4">
+                                <div class="small text-center"><a class="btn btn-block btn-primary" href="multi-tenant-create.html">Create new organization</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Join Organization-->
+                    <div class="col-xl-5 col-lg-6 col-md-8 col-sm-11 mt-4">
+                        <div class="card text-center h-100">
+                            <div class="card-body px-5 pt-5 d-flex flex-column align-items-between">
+                                <div>
+                                    <div class="h3 text-secondary">Join</div>
+                                    <p class="text-muted mb-4">Enter an access code or request access to an existing organization</p>
+                                </div>
+                                <div class="icons-org-join align-items-center mx-auto">
+                                    <i class="icon-user" data-feather="user"></i>
+                                    <i class="icon-arrow fas fa-long-arrow-alt-right"></i>
+                                    <i class="icon-users" data-feather="users"></i>
+                                </div>
+                            </div>
+                            <div class="card-footer bg-transparent px-5 py-4">
+                                <div class="small text-center"><a class="btn btn-block btn-secondary" href="multi-tenant-join.html">Join an organization</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+{{-- <div id="layoutAuthentication">
+    <div id="layoutAuthentication_content">
+        <main>
+            <div class="container-xl px-4">
+                <div class="row justify-content-center">
                     <div class="col-lg-7">
                         <!-- Basic registration form-->
                         <div class="card shadow-lg border-0 rounded-lg mt-5">
@@ -79,7 +125,7 @@
             </div>
         </footer>
     </div>
-</div>
+</div> --}}
 {{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-xl-10 col-lg-12 col-md-9">
@@ -148,4 +194,6 @@
         </div>
     </div>
 </div> --}}
+
 @endsection
+<script src="https://sb-admin-pro.startbootstrap.com/js/scripts.js"></script>
